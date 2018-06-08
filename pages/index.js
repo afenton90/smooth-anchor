2f97bc66df1f3e9820307fe040aa494017eecde0
Fenton, Alexander (Software Engineer) <alexander.fenton@sky.uk>
2018-06-08 23:58:16 +0100

    added title and fixed link references

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const SmoothHero = () => (
         <div className="columns is-mobile">
           <div className="column is-one-fifth is-offset-two-fifths">
             <Image
-              src="/static/anchor-shape.svg"
+              src="/smooth-anchor/static/anchor-shape.svg"
               alt="Smooth anchor logo"
               square
             />
@@ -72,6 +72,7 @@ const Hero3 = () => (
 export default () => (
   <Fragment>
     <Head>
+      <title>smooth-anchor | smoothly anchoring</title>
       <meta
         name="description"
         content="smooth-anchor polyfill for smooth scrolling web pages"
@@ -85,71 +86,71 @@ export default () => (
       <link
         rel="apple-touch-icon"
         sizes="57x57"
-        href="/static/apple-icon-57x57.png"
+        href="/smooth-anchor/static/apple-icon-57x57.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="60x60"
-        href="/static/apple-icon-60x60.png"
+        href="/smooth-anchor/static/apple-icon-60x60.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="72x72"
-        href="/static/apple-icon-72x72.png"
+        href="/smooth-anchor/static/apple-icon-72x72.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
-        href="/static/apple-icon-76x76.png"
+        href="/smooth-anchor/static/apple-icon-76x76.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="114x114"
-        href="/static/apple-icon-114x114.png"
+        href="/smooth-anchor/static/apple-icon-114x114.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="120x120"
-        href="/static/apple-icon-120x120.png"
+        href="/smooth-anchor/static/apple-icon-120x120.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="144x144"
-        href="/static/apple-icon-144x144.png"
+        href="/smooth-anchor/static/apple-icon-144x144.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="152x152"
-        href="/static/apple-icon-152x152.png"
+        href="/smooth-anchor/static/apple-icon-152x152.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/static/apple-icon-180x180.png"
+        href="/smooth-anchor/static/apple-icon-180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/static/android-icon-192x192.png"
+        href="/smooth-anchor/static/android-icon-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/static/favicon-32x32.png"
+        href="/smooth-anchor/static/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/static/favicon-96x96.png"
+        href="/smooth-anchor/static/favicon-96x96.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/static/favicon-16x16.png"
+        href="/smooth-anchor/static/favicon-16x16.png"
       />
       <meta name="robots" content="noarchive" />
       <meta name="theme-color" content="#ffffff" />
